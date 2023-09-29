cfb2c90bff144f2bd28df8725053b32fbe9dca4e
NEAmico <lishay.matvey20001506@gmail.com>
2023-09-29 14:49:40 +0200

    remove message after connect

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,7 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  await app.listen(port, '0.0.0.0', () => {
-    console.log('ekek');
-  });
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
